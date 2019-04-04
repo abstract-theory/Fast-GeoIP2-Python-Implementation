02daee50d97078be50a88bc69c54c72339a34aee
abstract-thoery <none>
2019-04-03 20:47:26 -0700

    Cleaned up code and re-organized. Added zip compression. Fixed bug. Added "progress" class.

--- src/lookup.py
+++ src/lookup.py
@@ -1,30 +1,32 @@
 
 
 import pickle
-
+import gzip
 
 def loadPreparedData(input_data):
-    with open(input_data, 'rb') as f:
-        cityLUT = pickle.loads(f.read())        
+    with gzip.open(input_data, 'rb') as f:
+        cityLUT = pickle.loads(f.read())
     return cityLUT
 
 def getLocations(ipDict, ips):
     country = [None] * len(ips)
     region = [None] * len(ips)
-    city = [None] * len(ips)    
-    for n in range(len(ips)):                
-        ip_s = ips[n].split('.')    
-        ipInt3 = int(ip_s[0])*65536 + int(ip_s[1])*256 + int(ip_s[2])        
+    city = [None] * len(ips)
+    for n in range(len(ips)):
+        ip_s = ips[n].split('.')
+        ipInt3 = int(ip_s[0])*65536 + int(ip_s[1])*256 + int(ip_s[2])
+        ipInt4 = int(ip_s[3])
+
         while True:
             try:
-                country[n], region[n], city[n] = ipDict[ipInt3]                
+                ip_blocks = ipDict[ipInt3]
+                for ip_block in reversed(ip_blocks):
+                    if ipInt4 >= ip_block[0]:
+                        country[n], region[n], city[n] = ip_block[1:4]
+                        break
                 break
             except KeyError:
                 ipInt3 -= 1
-            except ValueError:                             
-                ipInt4 = int(ip_s[3])                
-                for n1 in range(len(ipDict[ipInt3])):                    
-                    if ipInt4 >= ipDict[ipInt3][n1][0]:                       
-                        country[n], region[n], city[n] = ipDict[ipInt3][n1][1]                        
-                break                   
-    return country, region, city    
+
+    return country, region, city
+
--- src/precompute.py
+++ src/precompute.py
@@ -1,25 +1,85 @@
 
 import pickle
+import gzip
 import csv
-from collections import defaultdict, OrderedDict
 
 
-def createCityData(blocks_file_name, locs_file_name, output_file_name):        
+class prgs:
+    """A class to print program progress durring loops."""
+
+    def __init__(self, total, updates=None, loops=None):
+        self.inc = 0
+        self.loops = 0
+        self.count = 0
+        self.progress = 0
+        self.end = ''
+        self.fmt_str = None
+        self.deci = None
+        self.set(total, updates, loops)
+
+    def set_format(self, updates):
+        # deci = ceil [ log10(updates) ] - 2
+        self.deci = 0
+        while updates > 1:
+            updates /= 10
+            self.deci += 1
+        self.deci = int(self.deci) - 2
+        # deci can't be negative
+        if self.deci < 0:
+            self.deci = 0
+        # total length of float string: len('100.') = 4
+        total = self.deci + 4
+        # 2nd order string interpolation to format output
+        self.fmt_str = "\033[K\rprogress: %%%d.%df%%%%" % (total, self.deci)
+
+    def set(self, total, updates=None, loops=None):
+        if updates == 0 or loops == 0:
+            print("Error: input argument must not be zero.")
+            raise ValueError
+        if (not updates) == (not loops):
+            print("Error: you must specify exactly one of the following input variables: updates, loops.")
+            raise RuntimeError
+        if loops:
+            self.loops = loops
+            updates = total//self.loops
+        else:
+            # This must be an integer.
+            self.loops = total//updates
+            updates = total//self.loops
+        # Set the minimum number of decimal places that will change on each iteration.
+        self.set_format(updates)
+        # 1) After the last increment, progress must straddle the 100% mark.
+        # 2) By carefuly choosing "inc", the format string will auto-magically
+        #    round the final progress level to exactly 100%.
+        self.inc = ( 100+0.1*10**-self.deci ) / updates
+
+    def update(self):
+        self.count += 1
+        if self.count == self.loops:
+            self.progress += self.inc
+            self.count = 0
+            if self.progress >= 100:
+                self.end = '\n'
+            print(self.fmt_str % self.progress, end=self.end)
+
+
+
+def createCityData(blocks_file_name, locs_file_name, output_file_name):
     blocks = loadRawCsvData(blocks_file_name)[1:]
-    locs = loadRawCsvData(locs_file_name)[1:]    
+    locs = loadRawCsvData(locs_file_name)[1:]
     refs = geoRefs(blocks)
     names = locNames(locs)
-    cityLUT3 = ipLoc(refs, names)    
-    print("writing new data...")  
-    data = pickle.dumps(cityLUT3)
-    with open(output_file_name, 'wb') as f:
-        f.write(data)
+    cityLUT = ipLoc(refs, names)
+
+    print("writing new data...")
+    with gzip.open(output_file_name, 'wb') as f:
+        f.write(pickle.dumps(cityLUT, pickle.HIGHEST_PROTOCOL))
 
 
 def loadRawCsvData(fn):
     print("loading file %s..." % (fn))
-    with open(fn, 'r') as f:    
-        blocks = list(csv.reader(f))    
+    with open(fn, 'r') as f:
+        blocks = list(csv.reader(f))
     return blocks
 
 
@@ -28,27 +88,19 @@ def ipLoc(gRefs, lNames):
     maps ip address to location
     '''
     print("putting it all together...")
-    count = 0
-    l = len(gRefs)
     d = dict()
-    count_b = 0
-    for ref in gRefs:        
+    status = prgs(len(gRefs), loops=1000)
+    for ref in gRefs:
         blk = gRefs[ref]
-        try:
-            d[ref] = lNames[blk]
-        except TypeError:
-            d[ref] = []
-            for k in blk:
-                d[ref].append([k[0], lNames[k[1]]])                
-        count += 1
-        count_b += 1
-        if count_b == 1000:
-            count_b = 0
-            print("\033[K\rprogress: %5.2f%%" % (100*count/l), end='')
-            
-    print("\033[K\rprogress: %5.2f%%" % (100*count/l))
-    return d    
-
+        d[ref] = []
+        for k in blk:
+            try:
+                locationNames = lNames[k[1]]
+            except KeyError:
+                locationNames = [None,None,None,]
+            d[ref].append([k[0]] + locationNames)
+        status.update()
+    return d
 
 
 # 0: geoname_id,
@@ -72,10 +124,8 @@ def locNames(locs):
     print("processing locations...")
     d = dict()
     l = len(locs)
-    count_b = 0
-
     str_or_None = lambda s: None if s == '' else s
-    
+    status = prgs(l, loops=1000)
     for n in range(l):
         geoRef = int(locs[n][0])
         # Choose desired geographic info here. Refer to list above function.
@@ -84,12 +134,7 @@ def locNames(locs):
             str_or_None(locs[n][7]),
             str_or_None(locs[n][10]),
         ]
-        count_b += 1
-        if count_b == 999:
-            count_b = 0
-            print("\033[K\rprogress: %5.2f%%" % (100*n/l), end='')
-            
-    print("\033[K\rprogress: %5.2f%%" % (100*n/l))
+        status.update()
     return d
 
 
@@ -107,57 +152,25 @@ def geoRefs(blocks):
     '''
     returns dictionary that maps IP address (as an integer) to "geoname_id"
     '''
-    print("processing blocks...")    
-    d3 = dict()    
+    print("processing blocks...")
     l = len(blocks)
-    count_b = 0
-    count = 0
-    
-    dl = defaultdict(lambda: list())    
-    
-    for n in range(l):                
+    dl = dict()
+    status = prgs(l, loops=10000)
+    for n in range(l):
         ipStr = blocks[n][0].split('/')[0]
-        ip_s = ipStr.split('.')    
+        ip_s = ipStr.split('.')
         ip3 = int(ip_s[0])*65536 + int(ip_s[1])*256 + int(ip_s[2])
         ip4 = int(ip_s[3])
+        # Choose desired data here. Index referes to notes above function.
         try:
-            # Choose desired data here. Index referes to notes above function.
-            dl[ip3].append([ip4, int(blocks[n][1])])
+            mm_id = int(blocks[n][1])
         except ValueError:
-            # exception if block has no data
-            pass
-
-        if count_b == 9999:
-            count_b = 0
-            print("\033[K\rprogress: %5.2f%%" % (100*count/l), end='')
-        count_b += 1            
-        count += 1
-    print("\033[K\rprogress: %5.2f%%" % (100*count/l))
-    
-    print("processing more blocks...")    
-    l = len(dl)
-    count_b = 0
-    count = 0                
-    for ipl in dl:
-        if len(dl[ipl]) == 1 and dl[ipl][0][0] == 0:            
-            blk = dl[ipl][0][1]            
-            d3[ipl] = blk
-        else:
-            od = OrderedDict()
-            for n in range(len(dl[ipl])):            
-                ip4 = dl[ipl][n][0]
-                blk = dl[ipl][n][1]
-                od[ip4] = blk            
-            od = sorted(od.items(), key=lambda x: x[0])
-            d3[ipl] = od
-                            
-        if count_b == 9999:
-            count_b = 0
-            print("\033[K\rprogress: %5.2f%%" % (100*count/l), end='')
-        count_b += 1
-        count += 1
-    print("\033[K\rprogress: %5.2f%%" % (100*count/l))
-    
-    return d3     
+            mm_id = None
+        try:
+            dl[ip3].append([ip4, mm_id])
+        except KeyError:
+            dl[ip3] = [[ip4, mm_id]]
+        status.update()
+    return dl
 
 
