f4509a284e8b58e34e06c3197b789e76a23144fc
abstract-thoery <none>
2019-04-17 10:16:29 -0700

    Update filenames name to *.gz

--- src/example.py
+++ src/example.py
@@ -4,7 +4,7 @@ import precompute
 
 blocks = '/path/GeoLite2-City-Blocks-IPv4.csv'
 locs = '/path/GeoLite2-City-Locations-en.csv'
-output = '/path/ip_locations.pickle'
+output = '/path/ip_locations.pickle.gz'
 
 precompute.createCityData(blocks, locs, output)
 
@@ -12,7 +12,7 @@ precompute.createCityData(blocks, locs, output)
 # location look-up example
 import lookup
 
-input_data = '/path/ip_locations.pickle'
+input_data = '/path/ip_locations.pickle.gz'
 ip_city = lookup.loadPreparedData(input_data)
 ips = ['1.0.0.0', '1.0.0.0',]
 
